6fec3eada833c2353000209207bc03b91a7bf61f
PANAGIWTIS PETROPOULAKIS <sdi1500129@linux01.di.uoa.gr>
2018-09-18 13:11:07 +0300

    Fix-Comments-utils.h

--- mpi/utils.h
+++ mpi/utils.h
@@ -14,15 +14,15 @@
 
 /* Definitions */
 #define MAX_WIDTH 3500 // Max width of pictures
-#define MAX_HEIGHT 3500 // Max Height of pictures
+#define MAX_HEIGHT 3500 // Max height of pictures
 #define MAX_SEED 50 // For randomly generated pictures
 #define FILTER_SIZE 3 // 3 x 3 -> Must not be changed
-#define FILTER_MAX_VALUE 10 // Max value for filter
+#define FILTER_MAX_VALUE 10 // Max value of filter
 #define FILTER_MIN_VALUE -10
 #define MAX_ITERATIONS 200 
 
-/* Neighbours */
-#define NUM_NEIHGBOURS 8
+/* Neighbours - Mapping(nort, east, etc.) */
+#define NUM_NEIGHBOURS 8
 #define N 0
 #define NE 1
 #define E 2
@@ -32,25 +32,23 @@
 #define W 6
 #define NW 7
 
-/*  Header file for all variant functions and structs used
- *  to complete the Parallel Convolution application.
- */
+/*  Header file for all variant functions and structs used */
+/*  to complete the Parallel Convolution application       */
 
-
- /* Arguments struct for each mpi process */
- typedef struct Args_type{
+/* Arguments struct for each mpi process */
+typedef struct Args_type{
      int image_type, image_width, image_height, image_seed;
      double filter[FILTER_SIZE][FILTER_SIZE];
      int width_per_process, width_remaining;
      int height_per_process, height_remaining;
      int iterations;
- }Args_type;
+}Args_type;
 
-/*  Gets the filter of convolution. Given an already allocated array    */
-/*  scans from stdin the filter line by line. When the line is given    */
-/*  all integers are seperated and are put in filter. In case of        */
-/*  error, the line is rescanned.                                       */
+/*  Gets the filter of convolution. Given an allocated array         */
+/*  scans from stdin the filter line by line. When the line is given */
+/*  all integers are seperated and are placed in the filter. In case */
+/* of an error, the line is rescanned.                               */
 int read_filter(double[FILTER_SIZE][FILTER_SIZE]);
 
-/*  NOT FULLY CHECKED. LOOKS OKAY   */
+/* Read the arguments for convolution */
 int read_user_input(Args_type*, int procs_per_line);
